7273988afa961d50051201251007f45708f84368
biscoitinho <matgrotha@gmail.com>
2021-07-24 20:48:46 +0200

    add fixtures to tests

--- project/tests/test_basic.py
+++ project/tests/test_basic.py
@@ -1,26 +1,41 @@
+import pytest
 from project import create_app
 from project.models import User
 
-def test_home_page():
+@pytest.fixture(scope='module')
+def new_app():
+    flask_app = create_app()
+    return flask_app
+
+def test_home_page(new_app):
     """
     GIVEN a Flask application configured for testing
     WHEN the '/' page is called (GET)
     THEN check that a '200' status code is returned
     """
-    flask_app = create_app()
 
-    with flask_app.test_client() as test_client:
+    with new_app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
 
-def test_login_page():
+def test_login_page(new_app):
     """
     GIVEN a Flask application configured for testing
     WHEN the '/profile' page is called (GET)
     THEN check that a '200' status code is returned
     """
-    flask_app = create_app()
 
-    with flask_app.test_client() as test_client:
+    with new_app.test_client() as test_client:
         response = test_client.get('/login')
+        assert response.status_code == 200
+
+def test_profile_page(new_app):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/profile' page is called (GET)
+    THEN check that a '200' status code is returned
+    """
+
+    with new_app.test_client() as test_client:
+        response = test_client.get('/signup')
         assert response.status_code == 200
--- project/tests/test_models.py
+++ project/tests/test_models.py
@@ -1,42 +1,49 @@
+import pytest
 from project import create_app
 from project.models import User, Ttable, Task
 
-def test_user_model():
+@pytest.fixture(scope='module')
+def new_user():
+    user = User(email='joedoe@example.com', name='Mike', password='admin')
+    return user
+
+@pytest.fixture(scope='module')
+def new_app():
+    flask_app = create_app()
+    return flask_app
+
+def test_user_model(new_user, new_app):
     """
     GIVEN a User model
     WHEN a new User is created
     THEN check the email, hashed_password, and role fields are defined correctly
     """
-    flask_app = create_app()
 
-    with flask_app.test_client() as test_client:
-        user = User(email='joedoe@example.com', name='Mike', password='admin')
-        assert user.email == 'joedoe@example.com'
-        assert user.password == 'admin'
-        assert user.name == 'Mike'
+    with new_app.test_client() as test_client:
+        assert new_user.email == 'joedoe@example.com'
+        assert new_user.password == 'admin'
+        assert new_user.name == 'Mike'
 
-def test_ttable_model():
+def test_ttable_model(new_app):
     """
     GIVEN a Ttable model
     WHEN a new Ttable is created
     THEN check email and name fields are defined correctly
     """
-    flask_app = create_app()
 
-    with flask_app.test_client() as test_clinet:
+    with new_app.test_client() as test_clinet:
         ttable = Ttable(email='joedoe@example.com', name='Joe')
         assert ttable.email == 'joedoe@example.com'
         assert ttable.name == 'Joe'
 
-def test_task_model():
+def test_task_model(new_app):
     """
     GIVEN a Task model
     WHEN a new Task is created
     THEN check name, description and status fields are defined correctly
     """
-    flask_app = create_app()
 
-    with flask_app.test_client() as test_client:
+    with new_app.test_client() as test_client:
         task = Task(name='Test', description='some random stuff', status=1)
         assert task.name == 'Test'
         assert task.description =='some random stuff'
