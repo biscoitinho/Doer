23ebf87c3616f6c94047dcc0474edfa6b0b9f942
biscoitinho <matgrotha@gmail.com>
2021-08-15 21:06:19 +0200

    add CRUD tests for table

--- project/tests/test_crud_table.py
+++ project/tests/test_crud_table.py
@@ -60,6 +60,55 @@ def test_create_table(test_client, init_database):
     assert b"test table" in response.data
     assert b"Welcome, Joe here are your tables!" in response.data
 
+def test_edit_page(test_client, init_database):
+    """
+    GIVEN a Flask application
+    WHEN the '/tables/test_table/1/edit' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/tables/test_table/1/edit', follow_redirects=True)
+    assert response.status_code == 200
+
+def test_edit_table(test_client, init_database):
+    """
+    GIVEN a Flask application
+    WHEN the '/tables/test_table/1/edit' page is posted to (POST)
+    THEN check the response is valid
+    """
+    response = test_client.post('/tables/test_table/1/edit',
+                                data=dict(name='edit test table'),
+                                follow_redirects=True)
+    assert response.status_code == 200
+
+def test_check_edit(test_client, init_database):
+    """
+    GIVEN a Flask application
+    WHEN the '/tables' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/tables', follow_redirects=True)
+    assert response.status_code == 200
+    assert b"edit test table" in response.data
+
+def test_delete_table(test_client, init_database):
+    """
+    GIVEN a Flask application
+    WHEN the '/tables/edit_test_table/1/delete' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/tables/edit_test_table/1/delete', follow_redirects=True)
+    assert response.status_code == 200
+
+def test_check_delete_table(test_client, init_database):
+    """
+    GIVEN a Flask application
+    WHEN the '/tables/' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/tables', follow_redirects=True)
+    assert response.status_code == 200
+    assert b"edit test table" not in response.data
+
 def logout(test_client, init_database):
     """
     GIVEN a Flask application
